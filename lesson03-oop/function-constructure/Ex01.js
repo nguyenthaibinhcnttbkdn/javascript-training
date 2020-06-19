a1df6c83bc39edadf5005774045903373003030a
thaibinh <nguyenthaibinh16t2bkdn@gmail.com>
2020-06-20 00:16:08 +0700

    js-eloquen-lesson03-OOP 20.06.2020

---
+++ lesson03-oop/function-constructure/Ex01.js
@@ -0,0 +1,27 @@
+// function Person(name) {
+//     this.name = name;
+//     this.say = function() {
+//         console.log("My name is " + this.name);
+//     }
+// }
+
+// let peter = new Person('peter');
+// peter.say();
+
+
+const ThreeNumbers = function(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.sum = function() {
+        console.log('sum of three number: ' + (this.a + this.b + this.c));
+    }
+}
+let myNumber = new ThreeNumbers(12, 13, 14);
+myNumber.sum();
+
+ThreeNumbers.prototype.sub = function() {
+    console.log('sub of three number: ' + (this.a - this.b - this.c));
+}
+myNumber.sub();
+
